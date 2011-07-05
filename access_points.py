495bc638786e0aa14abd19ceeb76a6db3f4bb380
Amardine David <amardine.david@kozea.fr>
2011-07-05 16:13:55 +0200

    first commit

---
+++ access_points.py
@@ -0,0 +1,86 @@
+   ###########################################################################
+ ##                 _____           _         _     _                       ##
+##                 |  __ \         | |       | |   (_)                      ##
+##                 | |__) |__ _ ___| |_  __ _| |__  _ _ __                  ##
+##                 |  ___// _` / __| __|/ _` | '_ \| | '_ \                 ##
+##                 | |   | (_| \__ \ |_| (_| | |_) | | | | |                ##
+##                 |_|    \__,_|___/\__|\__,_|_.__/|_|_| |_|                ##
+##                                                                          ##
+##                        --  http://pastabin.org/  --                      ##
+##                                                                          ##
+##                                                                          ##
+## Pastabin - A pastebin with a lot of taste                                ##
+##                                                                          ##
+## Copyright (C) 2011  Kozea                                                ##
+##                                                                          ##
+## This program is free software: you can redistribute it and/or modify     ##
+## it under the terms of the GNU Affero General Public License as published ##
+## by the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                      ##
+##                                                                          ##
+## This program is distributed in the hope that it will be useful,          ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of           ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ##
+## GNU Affero General Public License for more details.                      ##
+##                                                                          ##
+## You should have received a copy of the GNU Affero General Public License ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.    ##
+##                                                                          ##
+##                                                                          ##
+## Authored by: Amardine DAVID <amardine.david@kozea.fr>                    ##
+## Authored by: Jérôme DEROCK <jerome.derock@kozea.fr>                      ##
+## Authored by: Fabien LOISON <fabien.loison@kozea.fr>                      ##
+                                                  ##
+## Pastabin - A pastebin with a lot of taste                                ##
+##                                                                          ##
+## Copyright (C) 2011  Kozea                                                ##
+##                                                                          ##
+## This program is free software: you can redistribute it and/or modify     ##
+## it under the terms of the GNU Affero General Public License as published ##
+## by the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                      ##
+##                                                                          ##
+## This program is distributed in the hope that it will be useful,          ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of           ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ##
+## GNU Affero General Public License for more details.                      ##
+##                                                                          ##
+## You should have received a copy of the GNU Affero General Public License ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.    ##
+##                                                                          ##
+##                                                                          ##
+## Authored by: Amardine DAVID <amardine.david@kozea.fr>                    ##
+## Authored by: Jérôme DEROCK <jerome.derock@kozea.fr>                      ##
+## Authored by: Fabien LOISON <fabien.loison@kozea.fr>                      ##
+##                                                                         ##
+###########################################################################
+from multicorn.corns.alchemy import Alchemy
+from multicorn.declarative import declare, Property
+
+def make_corn():
+    mc = Muliticorn()
+    corn_person = make_person_ap()
+    mc.register(corn_person)
+    corn_snippet = make_snippet_ap
+    mc.register(corn_snippet)
+    yield corn
+
+def make_person_ap():
+    @declare(Alchemy, identity_properties=("id", url="sqlite:///"))
+    class Person(object):
+        id = Property(type=int)
+        login = Property(type=unicode)
+        pwd = Property(type=unicode)
+        mail = Property(type=unicode)
+    return Person
+
+def make_snippet_ap():
+    @declare(Alchemy, identity_properties=("id",url="sqlite:///"))
+    class Snippet(object):
+        id = Property(type=int)
+        person_id = Property(type=int)
+        date = Property(type=unicode)
+        language = Property(type=unicode)
+        text = Property(type=unicode)
+        title = Property(type=unicode)
+    return Snippet
---
+++ pastabin.py
@@ -0,0 +1,43 @@
+   ###########################################################################
+ ##                 _____           _         _     _                       ##
+##                 |  __ \         | |       | |   (_)                      ##
+##                 | |__) |__ _ ___| |_  __ _| |__  _ _ __                  ##
+##                 |  ___// _` / __| __|/ _` | '_ \| | '_ \                 ##
+##                 | |   | (_| \__ \ |_| (_| | |_) | | | | |                ##
+##                 |_|    \__,_|___/\__|\__,_|_.__/|_|_| |_|                ##
+##                                                                          ##
+##                        --  http://pastabin.org/  --                      ##
+##                                                                          ##
+##                                                                          ##
+## Pastabin - A pastebin with a lot of taste                                ##
+##                                                                          ##
+## Copyright (C) 2011  Kozea                                                ##
+##                                                                          ##
+## This program is free software: you can redistribute it and/or modify     ##
+## it under the terms of the GNU Affero General Public License as published ##
+## by the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                      ##
+##                                                                          ##
+## This program is distributed in the hope that it will be useful,          ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of           ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ##
+## GNU Affero General Public License for more details.                      ##
+##                                                                          ##
+## You should have received a copy of the GNU Affero General Public License ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.    ##
+##                                                                          ##
+##                                                                          ##
+## Authored by: Amardine DAVID <amardine.david@kozea.fr>                    ##
+## Authored by: Jérôme DEROCK <jerome.derock@kozea.fr>                      ##
+## Authored by: Fabien LOISON <fabien.loison@kozea.fr>                      ##
+##                                                                         ##
+###########################################################################
+
+from multicorn.declarative import declare, Property
+from corns import Person, Snippet
+
+
+if __name__ == '__main__':
+    run()
+
+
