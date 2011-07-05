1106924161859d1b323b9fd00e6042b1474732f6
Amardine David <amardine.david@kozea.fr>
2011-07-05 17:16:17 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+   ###########################################################################
+ ##                 _____           _         _     _                       ##
+##                 |  __ \         | |       | |   (_)                      ##
+##                 | |__) |__ _ ___| |_  __ _| |__  _ _ __                  ##
+##                 |  ___// _` / __| __|/ _` | '_ \| | '_ \                 ##
+##                 | |   | (_| \__ \ |_| (_| | |_) | | | | |                ##
+##                 |_|    \__,_|___/\__|\__,_|_.__/|_|_| |_|                ##
+##                                                                          ##
+##                        --  http://pastabin.org/  --                      ##
+##                                                                          ##
+##                                                                          ##
+## Pastabin - A pastebin with a lot of taste                                ##
+##                                                                          ##
+## Copyright (C) 2011  Kozea                                                ##
+##                                                                          ##
+## This program is free software: you can redistribute it and/or modify     ##
+## it under the terms of the GNU Affero General Public License as published ##
+## by the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                      ##
+##                                                                          ##
+## This program is distributed in the hope that it will be useful,          ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of           ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ##
+## GNU Affero General Public License for more details.                      ##
+##                                                                          ##
+## You should have received a copy of the GNU Affero General Public License ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.    ##
+##                                                                          ##
+##                                                                          ##
+## Authored by: Amardine DAVID <amardine.david@kozea.fr>                    ##
+## Authored by: Jérôme DEROCK <jerome.derock@kozea.fr>                      ##
+## Authored by: Fabien LOISON <fabien.loison@kozea.fr>                      ##
+##                                                                         ##
+###########################################################################
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Pastabin",
+    packages=find_packages(),
+    install_requires=["Flask", "Multicorn"],
+    )
+
