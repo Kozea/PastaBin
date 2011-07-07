7af74a38b5a577fdb40e687d819c086932ff4925
Fabien LOISON <fabien.loison@kozea.fr>
2011-07-07 09:38:51 +0200

    Some code refactoring/cleanning in pastabin.py

--- pastabin.py
+++ pastabin.py
@@ -44,7 +44,6 @@ from multicorn.declarative import declare, Property
 from multicorn.requests import CONTEXT as c
 
 from access_points import *
-from multicorn.requests import CONTEXT as c
 
 
 app = Flask(__name__)
@@ -56,7 +55,22 @@ app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
-@app.route("/add", methods=("GET", "POST"))
+@app.route("/", methods=("GET",))
+def index():
+    data =  {"snippets" : Snippet.all.execute()}
+    return render_template("index.html.jinja2", **data)
+
+
+@app.route("/snippet/<int:snippet_id>", methods=("GET",))
+@app.route("/s/<int:snippet_id>", methods=["GET"])
+def get_snippet_by_id(snippet_id):
+    item = Snippet.all.filter(
+        c.id == snippet_id).one().execute()
+    data = {"snippet" : item}
+    return render_template("snippet.html.jinja2", **data)
+
+
+@app.route("/add", methods=["GET", "POST"])
 def add_snippet():
     if request.method == "POST":
         item = Snippet.create({
@@ -70,24 +84,47 @@ def add_snippet():
     else:
         return render_template("add.html.jinja2")
 
-@app.route("/", methods=("GET",))
-def index():
-    data =  {"snippets" : Snippet.all.execute()}
-    return render_template("index.html.jinja2", **data)
+
+@app.route("/modify/<int:id>", methods=["GET"])
+def modify_snippet_get(id):
+    if not session.get('logged_in'):
+        return redirect(url_for("connect"))
+    item = Snippet.all.filter(c.id == id).one(None).execute()
+    if item is not None:
+        return render_template(
+                "modify.html.jinja2",
+                snip_id=item['id'],
+                snip_title=item['title'],
+                snip_language=item['language'],
+                snip_text=item['text'],
+                )
+    else:
+        return "Groaaah!", 404
 
 
-@app.route("/snippet/<int:snippet_id>", methods=("GET",))
-def get_snippet_by_id(snippet_id):
-    item = Snippet.all.filter(
-        c.id == snippet_id).one().execute()
-    data = {"snippet" : item}
-    return render_template("snippet.html.jinja2", **data)
+@app.route("/modify/<int:id>", methods=["POST"])
+def modify_snippet_post(id):
+    if not session.get('logged_in'):
+        return redirect(url_for("connect"))
+    item = Snippet.all.filter(c.id == id).one(None).execute()
+    if item is not None:
+        item['date'] = datetime.now()
+        item['language'] = request.form['snip_language']
+        item['title'] = request.form['snip_title']
+        item['text'] = request.form['snip_text']
+        item.save()
+    return redirect(url_for("get_snippet_by_id", snippet_id=item['id']))
 
 
-@app.route('/connect', methods=['POST',])
+@app.route('/connect', methods=['GET'])
+def get_connect():
+    return render_template('connect.html.jinja2')
+
+
+@app.route('/connect', methods=['POST'])
 def connect():
     item = Person.all.filter(
-        c.login == request.form['login']).one(None)
+        c.login.lower() == request.form['login'].lower()).one(None)
     item = item.execute()
     if '' == request.form.get('login', '') \
         or '' == request.form.get('password', ''):
@@ -102,11 +139,6 @@ def connect():
         return redirect(url_for("connect"))
 
 
-@app.route('/connect', methods=('GET',))
-def get_connect():
-    return render_template('connect.html.jinja2')
-
-
 @app.route('/disconnect')
 def disconnect():
     session.pop('logged_in', None)
@@ -114,33 +146,7 @@ def disconnect():
     return redirect("/") #FIXME
 
 
-@app.route("/modify/<int:id>", methods=("GET", "POST"))
-def modify_snippet(id):
-    if not session.get('logged_in'):
-        return redirect(url_for("connect"))
-    if request.method == "POST":
-        item = Snippet.all.filter(c.id == id).one(None).execute()
-        if item is not None:
-            item['date'] = datetime.now()
-            item['language'] = request.form['snip_language']
-            item['title'] = request.form['snip_title']
-            item['text'] = request.form['snip_text']
-            item.save()
-        return redirect(url_for("get_snippet_by_id", snippet_id=item['id']))
-    else:
-        item = Snippet.all.filter(c.id == id).one(None).execute()
-        if item is not None:
-            return render_template(
-                    "modify.html.jinja2",
-                    snip_id=item['id'],
-                    snip_title=item['title'],
-                    snip_language=item['language'],
-                    snip_text=item['text'],
-                    )
-        else:
-            return "Groaaah!",404
-
-@app.route('/register', methods=('POST',))
+@app.route('/register', methods=['POST'])
 def register():
     if '' == request.form.get('login', '') \
         or '' == request.form.get('password', '') \
@@ -154,11 +160,13 @@ def register():
             }).save()
         return 'REGISTER'
 
-@app.route('/register', methods=('GET',))
+
+@app.route('/register', methods=['GET'])
 def get_register():
     return render_template('register.html.jinja2')
 
+
 if __name__ == '__main__':
-    app.run()
-#    app.run(debug=True)
+#    app.run()
+    app.run(debug=True)
 
